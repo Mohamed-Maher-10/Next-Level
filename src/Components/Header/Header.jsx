1869e4a332145dc32e52df44d789f9198e10eb55
Mohamed Maher <mohamedmaher002009@gmail.com>
2026-07-10 18:58:12 +0300

    Final Dark Mode

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,11 +5,17 @@ import './Header.css'
 export default function Header() {
   return (
     <div>
-
         <header>
-            <img src={logo} alt='logo' />
+            <div className="headerGlowBg"></div> {/* طبقة نيون متحركة في الخلفية */}
+            
+            <img src={logo} alt='logo' className="heroLogo" />
+            
+            <p className="heroText">
+              Empower Your Future With <span className="neonGlowText">Next-Generation</span> Learning
+            </p>
+            
+            <button className="heroBtn">Explore Courses ↓</button>
         </header>
-      
     </div>
   )
 }
