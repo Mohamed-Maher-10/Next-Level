00789cc981504acd7ce8438945204e5be462c927
Mohamed Maher <mohamedmaher002009@gmail.com>
2026-07-24 10:48:32 +0300

    after target

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <h2>
           Start Learning Now & Reach Your <span className="brand-name">Next Level</span> 🚀
         </h2>
-        <a href='https://docs.google.com/forms/d/e/1FAIpQLScWCYu5tbKWFDge73PTSXRePVGngxlqfjWZ69Cf1Om-M5JKEg/viewform?usp=header'>Sign Now</a>
+        <a href='https://docs.google.com/forms/d/e/1FAIpQLScWCYu5tbKWFDge73PTSXRePVGngxlqfjWZ69Cf1Om-M5JKEg/viewform?usp=header' target='_blank'>Sign Now</a>
     </footer>
 
     </div>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
               Empower Your Future With <span className="neonGlowText">Next-Generation</span> Learning
             </p>
             
-            <a className="heroBtn" href='https://docs.google.com/forms/d/e/1FAIpQLScWCYu5tbKWFDge73PTSXRePVGngxlqfjWZ69Cf1Om-M5JKEg/viewform?usp=header'>Start Learning Now</a>
+            <a className="heroBtn" href='https://docs.google.com/forms/d/e/1FAIpQLScWCYu5tbKWFDge73PTSXRePVGngxlqfjWZ69Cf1Om-M5JKEg/viewform?usp=header' target='_blank'>Start Learning Now</a>
         </header>
     </div>
   )
