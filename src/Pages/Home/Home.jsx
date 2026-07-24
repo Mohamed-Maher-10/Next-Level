2e4a5dcbe2f2ccbe0103da9637a706388052823e
Mohamed Maher <mohamedmaher002009@gmail.com>
2026-07-24 10:43:02 +0300

    webSite after added kids course

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import CourseCard from '../../Components/CourseCard/CourseCard'
 
 import logo1 from '../../assets/logo1.png'
 import logo2 from '../../assets/logo2.png'
+import kids from '../../assets/kids.jpg'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <div id='courses'>
         <CourseCard logo={logo1} title="Basic of Front End Web Development" desc="Kickstart your tech journey and master the core building blocks of the web. In this comprehensive course, you will dive into HTML5, CSS3, and JavaScript basics. You will learn how to structure, style, and bring websites to life from scratch, building a solid foundation for your future as a professional developer." />
         <CourseCard logo={logo2} title="Advanced Front End" desc="Take your coding skills to the next level and build industry-grade web applications. Master advanced JavaScript concepts, state management, and modern component-based development with React. You will learn clean architecture, performance optimization, and API integration to build ultra-fast, responsive, and dynamic web experiences." />
+        <CourseCard logo={kids} title="Junior Coders Program" desc="Empower your child with essential coding skills. From logic building with Scratch and core programming concepts in C++, to real-world software creation in Python—all through fun, hands-on projects." />
       </div>
       
 
